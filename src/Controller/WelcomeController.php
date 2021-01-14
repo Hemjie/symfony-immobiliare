2569c6a4999f6ced26d8faf369db593dac4566db
Hemjie <marjolaine.vilain@gmail.com>
2021-01-14 12:06:38 +0100

    création de la route hello

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -5,10 +5,17 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController
 {
-    public function hello() {
-        return new Response();
+    /**
+     * @Route("/hello", name="hello")
+     */
+    //name sert à renommer la route
+    // "/hello" pour l'url
+    public function hello() { //bien mettre le code pour une route juste en-dessous
+        // on renvoie tjs un objet Response
+        return new Response('<html><body>Hello Symfony</body></html>'); //balise body pour que la toolbar fonctionne tjs
     }
 }
