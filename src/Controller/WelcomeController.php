97174305bda8fd10dc7711de5657a17ced1dfe1a
Hemjie <marjolaine.vilain@gmail.com>
2021-01-14 13:44:57 +0100

    création de la vue hello avec la méthode render

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WelcomeController
+class WelcomeController extends AbstractController //permet d'utiliser le render
 {
     /**
      * @Route("/hello", name="hello")
@@ -16,6 +17,7 @@ class WelcomeController
     // "/hello" pour l'url
     public function hello() { //bien mettre le code pour une route juste en-dessous
         // on renvoie tjs un objet Response
-        return new Response('<html><body>Hello Symfony</body></html>'); //balise body pour que la toolbar fonctionne tjs
+        // return new Response('<html><body>Hello Symfony</body></html>'); //balise body pour que la toolbar fonctionne tjs
+        return $this->render('Welcome/hello.html.twig'); //le chemin templates est déjà dans le render
     }
 }
