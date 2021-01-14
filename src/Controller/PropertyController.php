2d7f67404f620300a37a5eb67ec5cbd2d17dab6f
Hemjie <marjolaine.vilain@gmail.com>
2021-01-14 16:34:29 +0100

    personnalisation des urls et boucles en twig

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -10,11 +10,35 @@ class PropertyController extends AbstractController
 {
     /**
      * @Route("/property", name="property")
+     *
+     * Page qui liste les annonces immobilières
      */
     public function index(): Response
     {
+        //Pour démarrer, on va créer un tableau d'annonces
+        $properties = [
+            [ 'title' => 'Maison avec piscine'],
+            [ 'title' => 'Appartement avec terrasse'],
+            [ 'title' => 'Studio centre ville']
+        ];
+
+        //Equivalent du var_dump
+        dump($properties);
+
         return $this->render('property/index.html.twig', [
-            'controller_name' => 'PropertyController',
+            'properties' => $properties,
+        ]);
+    }
+
+    /**
+     * @Route("property/{slug}", name="property_show")
+     *
+     * Page qui affiche une annonce avec un paramètre dynamique (ici slug)
+     */
+    public function show($slug): Response
+    {
+        return $this->render('property/show.html.twig', [
+            'slug' => $slug,
         ]);
     }
 }
