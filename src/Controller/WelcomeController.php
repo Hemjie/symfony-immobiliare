0d8331ac81023b566f710c96611aa76fad907f0f
Hemjie <marjolaine.vilain@gmail.com>
2021-01-14 14:52:50 +0100

    création d'un nouveau controller et d'une page avec le terminal

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -15,7 +15,8 @@ class WelcomeController extends AbstractController //permet d'utiliser le render
      */
     //name sert à renommer la route
     // "/hello" pour l'url
-    public function hello() { //bien mettre le code pour une route juste en-dessous
+    public function hello() : Response { // : Response permet typage de la fonction
+        //bien mettre le code pour une route juste en-dessous
         // on renvoie tjs un objet Response
         // return new Response('<html><body>Hello Symfony</body></html>'); //balise body pour que la toolbar fonctionne tjs
 
