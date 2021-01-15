9d70a2d1f33d85fc0798997b6a709d3877f21c86
Hemjie <marjolaine.vilain@gmail.com>
2021-01-15 08:40:09 +0100

    création d'une route en json

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -8,19 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+    // Pour partager le tableau dans toute la classe
+    private $properties = [
+        [ 'title' => 'Maison avec piscine'],
+        [ 'title' => 'Appartement avec terrasse'],
+        [ 'title' => 'Studio centre ville']
+    ];
+
     /**
-     * @Route("/property", name="property")
+     * @Route("/property/{page}", name="property_list", requirements={"page"="\d+"})
      *
      * Page qui liste les annonces immobilières
+     * requirement permet de vérifier que page est un nombre avec une regex
+     * si ce n'est pas un nombre, il fera la fonction show
      */
-    public function index(): Response
+    public function index($page = 1): Response //fixer la valeur par défaut éviter les erreurs
     {
         //Pour démarrer, on va créer un tableau d'annonces
-        $properties = [
-            [ 'title' => 'Maison avec piscine'],
-            [ 'title' => 'Appartement avec terrasse'],
-            [ 'title' => 'Studio centre ville']
-        ];
+        $properties = $this->properties;
 
         //Equivalent du var_dump
         dump($properties);
@@ -37,8 +42,23 @@ class PropertyController extends AbstractController
      */
     public function show($slug): Response
     {
+        // Ici, on peut vérifier que le slug soit dans notre tableau properties
+        if(!in_array($slug, array_column($this->properties, 'title'))) {
+            // permet de lever une exception, on arrête le code et on renvoie une erreur.
+            //symfony fera un try catch et renverra une 404
+            throw $this->createNotFoundException('Annonce non existante');
+        }
+
         return $this->render('property/show.html.twig', [
             'slug' => $slug,
         ]);
+    }
+
+    /**
+     * @Route("/property.json", name="property_api")
+     */
+    public function api(): Response
+    {
+        return $this->json($this->properties);
     }
 }
