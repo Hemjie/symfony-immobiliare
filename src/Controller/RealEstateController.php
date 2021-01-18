d9172e00c838c0be0f41a8df49492ddf4ad3c227
Hemjie <marjolaine.vilain@gmail.com>
2021-01-18 15:01:07 +0100

    validation d'un formulaire

--- src/Controller/RealEstateController.php
+++ src/Controller/RealEstateController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\RealEstate;
 use App\Form\RealEstateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,11 +24,30 @@ class RealEstateController extends AbstractController
     /**
      * @Route("/creer_un_bien", name="real_estate_create")
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
         // Avec Symfony, on peut créer un formulaire
         // Le formulaire est tjs dans une classe à part
-        $form = $this->createForm(RealEstateType::class);
+        // Dans la plupart des cas, on passe une entité à un formulaire
+        $realEstate = new RealEstate(); //use App\Entity\RealEstate;
+        $form = $this->createForm(RealEstateType::class, $realEstate);
+
+        //Il faut lier le formulaire à la requête (pour récupérer $_POST)
+        $form->handleRequest($request);
+
+        // On doit vérifier que le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Ici, on ajoute l'annonce dans la base...
+            //$realEstate = $form->getData() : pour récupérer les données du formulaire
+            dump($realEstate);
+
+            //Je dois ajouter l'objet dans la BDD
+            $entityManager = $this->getDoctrine()->getManager();
+            //Je dois mettre l'objet en attente
+            $entityManager->persist($realEstate);
+            //Exécuter la requête
+            $entityManager->flush();
+        }
 
         return $this->render('real_estate/create.html.twig', [
             //Pour afficher le formulaire
