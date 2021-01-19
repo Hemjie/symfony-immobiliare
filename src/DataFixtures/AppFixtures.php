40ba2b95b56baa52045df842e6b53461749a6693
Hemjie <marjolaine.vilain@gmail.com>
2021-01-19 16:33:22 +0100

    ajout du composer faker et créations de données aléatoires dans la BDD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,20 +5,29 @@ namespace App\DataFixtures;
 use App\Entity\RealEstate;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // On crée une instance de Faker pour générer la donnée aléatoire
+        $faker = Factory::create('fr_FR');
+
         for ($i = 1; $i <= 100; $i++) {
             $realEstate = new RealEstate();
-            $realEstate->setTitle('Un appartement fixtures');
-            $realEstate->setDescription('Une description');
-            $realEstate->setSurface(50);
-            $realEstate->setPrice(100000);
-            $realEstate->setRooms(4);
-            $realEstate->setType('appartement');
-            $realEstate->setSold(false);
+            $type = $faker->randomElement(['maison', 'appartement']);
+            $title = ucfirst($type).' '; // Appartement ou Maison
+            $rooms = $faker->numberBetween(1, 5);
+            $title .= RealEstate::SIZES[$rooms]; //Création de la constante SIZES, on obtient les valeurs Studio, T2...
+            // Maison T4 (en centre-ville, en campagne)
+            $realEstate->setTitle($title);
+            $realEstate->setDescription($faker->text);
+            $realEstate->setSurface($faker->numberBetween(10, 400));
+            $realEstate->setPrice($faker->numberBetween(34875, 584725));
+            $realEstate->setRooms($faker->numberBetween(1, 5));
+            $realEstate->setType($type);
+            $realEstate->setSold($faker->boolean(10)); //10 % de chances d'avoir true
 
             $manager->persist($realEstate); // On a crée une annonce
         }
