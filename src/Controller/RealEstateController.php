30537a5015145eb157f1bcec14e0e4c01e18797c
Hemjie <marjolaine.vilain@gmail.com>
2021-01-19 14:31:34 +0100

    lien list et ajout, messages flash

--- src/Controller/RealEstateController.php
+++ src/Controller/RealEstateController.php
@@ -25,12 +25,40 @@ class RealEstateController extends AbstractController
             4 => 'T4',
             5 => 'T5'
         ];
-        $properties = [];
+        // On appelle le dépôt d'une entité (là où sont stockées les entités)
+        $repository = $this->getDoctrine()->getRepository(RealEstate::class);
+        // Équivaut à SELECT * FROM real_estate
+        $properties = $repository->findAll();
 
         return $this->render('real_estate/index.html.twig', [
             'sizes' => $sizes,
             'properties' => $properties,
         ]);
+    }
+
+    /**
+     * @Route("/nos-biens/{id}", name="real_estate_show")
+     *
+     * URL de la page qui affiche un bien
+     */
+    public function show(RealEstate $property) //ainsi symfony gère la BDD et la page 404
+    {
+        // Avec @ParamConverter, on n'a pas besoin du code suivant
+        // Il suffit de typer le paramètre avec l'entité que l'on souhaite récupérer
+
+        // Si show($id), on récupère la propriété en BDD
+        // $property = $this->getDoctrine()->getRepository(RealEstate::class)
+        //   ->find($id);
+
+        // Renvoie une 404 si la propriété n'existe pas
+        // if (!$property) {
+        //    throw $this->createNotFoundException();
+        // }
+
+        return $this->render('real_estate/show.html.twig', [
+            'property' => $property,
+            'title' => $property->getTitle(),
+        ]); //chemin vers le fichier
     }
 
     /**
@@ -47,6 +75,7 @@ class RealEstateController extends AbstractController
         //Il faut lier le formulaire à la requête (pour récupérer $_POST)
         $form->handleRequest($request);
 
+
         // On doit vérifier que le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
             // Ici, on ajoute l'annonce dans la base...
@@ -59,6 +88,14 @@ class RealEstateController extends AbstractController
             $entityManager->persist($realEstate);
             //Exécuter la requête
             $entityManager->flush();
+
+            // Faire une redirection après l'ajout et afficher un message de succès
+            $this->addFlash('success', 'Votre annonce '.$realEstate->getId().' a bien été créée');
+
+            //Faire redirection vers la liste des annonces
+            return $this->redirectToRoute("real_estate_list");
+            //Afficher le msg flash dans le html
+
         }
 
         return $this->render('real_estate/create.html.twig', [
