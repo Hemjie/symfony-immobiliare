994463aa9b8c74d6c1290363b67e1924cdf6f98e
Hemjie <marjolaine.vilain@gmail.com>
2021-01-26 08:56:38 +0100

    gestion des users dans EasyAdmin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            'email', ArrayField::new('roles'),
+        ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        //Cette ligne permet de cacher le fait de pouvoir créer un user dans la BDD
+        return $actions->remove(Crud::PAGE_INDEX, Action::NEW);
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
