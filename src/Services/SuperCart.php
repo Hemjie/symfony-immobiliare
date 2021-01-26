a2c7c47ab8ca2f187ebaa88f5c49ddbf05960b10
Hemjie <marjolaine.vilain@gmail.com>
2021-01-26 15:45:18 +0100

    création d'un service pour le panier

---
+++ src/Services/SuperCart.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+/**
+ * Un service est une classe dont l'objectif est d'être réutilisable. Elle sert aussi à organiser son code
+ */
+
+class SuperCart
+{
+    private $session;
+
+    /**
+     * On peut récupérer d'autres services dans le constructeur d'un service
+     */
+
+    public function __construct(SessionInterface $session) {
+        $this->session = $session;
+    }
+
+    /**
+     * Permet ajout d'un item dans le panier
+     */
+    public function addItem($item) {
+        //Retourne le tableau de produits présents en session OU un tableau vide
+        $products = $this->session->get('products', []);
+        $products[$item->getId()]= $item; //L'index évite les doublons
+        $this->session->set('products', $products);
+    }
+
+    /**
+     * Permet de récupérer les items du panier
+     */
+    public function getItems() {
+        return $this->session->get('products', []);
+    }
+
+    /**
+     * Permet de compter les items dans le panier
+     */
+    public function count(): int
+    {
+        return count($this->session->get('products', []));
+    }
+}
